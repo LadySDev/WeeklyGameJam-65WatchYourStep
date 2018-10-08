a3c074d441c02682e673ea0b5a0d5282dd9b4a34
LadyS <ladysvideogame@gmail.com>
2018-10-08 10:59:42 +0200

    GameManager ShowTitle

---
+++ WGJ#65WatchYourStep/Assets/Scripts/ScrGameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrGameManager : MonoBehaviour {
+
+    private GameObject canva;
+
+    [SerializeField]
+    private GameObject panelTitle;
+
+	// Use this for initialization
+	void Start () {
+
+        canva = GameObject.Find("Canvas");
+
+        ShowTitle();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowTitle()
+    {
+        Instantiate(panelTitle, canva.transform);
+    }
+
+}
